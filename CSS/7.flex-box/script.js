c5c8923667b3a1c691c12877b9209bbd29ec79d8
Arjun Kumar <arjunadps662@gmail.com>
2025-08-25 11:55:58 +0530

    Add files via upload

---
+++ CSS/7.flex-box/script.js
@@ -0,0 +1,13 @@
+const container = document.getElementById("flex-container");
+
+function setDirection(dir) {
+  container.style.flexDirection = dir;
+}
+
+function setJustify(justify) {
+  container.style.justifyContent = justify;
+}
+
+function setAlign(align) {
+  container.style.alignItems = align;
+}
